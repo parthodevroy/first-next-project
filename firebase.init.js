533e7d6177046773d4919c180c17050392b20da4
unknown <you@example.com>
2025-11-25 12:43:37 +0600

    add env loal file

--- firebase.init.js
+++ firebase.init.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLc5fh84ACQLWNIm51Uz6-MIr1NCvGYGE",
-  authDomain: "my-first-next-9f136.firebaseapp.com",
-  projectId: "my-first-next-9f136",
-  storageBucket: "my-first-next-9f136.firebasestorage.app",
-  messagingSenderId: "140707580446",
-  appId: "1:140707580446:web:2c8be318932176b88dbc0f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
